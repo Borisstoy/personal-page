7adcf75ebf9688170e4e203201203e1b9e6021ea
BorisstoyE <boris.stoy@gmaiL.com>
2016-11-27 15:56:18 +0100

    Removed devise sign up functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 	resources :contacts, only: [:new, :create]
 	get 'welcome/index'
 	root 'welcome#index'
+
+	# trick to avoid routing error when people do : /whatever_not_allowed
+	get '*path' => redirect('/')
 end
